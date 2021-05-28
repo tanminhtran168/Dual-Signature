8e35b9eacfe907d2ba20c43aa7ed6ec056665c98
TranTanMinh <tanminhtran168@gmail.com>
2021-05-28 07:58:27 +0700

    fix

--- implem.py
+++ implem.py
@@ -1,130 +1,61 @@
-#####################################################
-#ICS MINI-PROJECT(Concept of Dual Signature)
-#####################################################
-
 from flask import Flask,redirect,url_for,request
-from flask_restful import Resource, Api
-import random
+from flask_cors import CORS
+from hashlib import sha1
+import rsa
 
 app=Flask(__name__)
-api = Api(app)
+CORS(app)
 
-class test(Resource):
-    def post(self):
-        return "minh"
+@app.route('/sha_order', methods=['POST'])
+def sha_order():
+    data = request.get_json()
 
-p,q = 7,17
-e,d = 0,0
+    proid = data['productid']
+    info = data['info']
+    cost = data['cost']
+    OI = str(proid) + str(info) + str(cost)
+    OIMD = sha1(OI.encode('utf-8')).hexdigest()
+    return OIMD
 
-def gcd(a,b):
-    while b!=0:
-        c=a%b
-        a=b
-        b=c
-    return a
+@app.route('/sha_payment', methods=['POST'])
+def sha_payment():
+    data = request.get_json()
 
-def mod(a,b,c):
-	ans = a
-	for i in range(1,b):
-			ans = (ans*a)%c
-	return (ans)%c
+    name = data['name']
+    cmnd = data['cmnd']
+    card = data['card']
+    PI = name + cmnd + card
+    PIMD = sha1(PI.encode('utf-8')).hexdigest()
+    return PIMD
 
-def encrypt(m,e,n):
-    return mod(m,e,n)
+@app.route('/sha_join', methods=['POST'])
+def sha_join():
+    data = request.get_json()
+    join_result = data['join_result']
 
-def decrypt(c):
-	global d
-	n = p*q
-	return mod(c,d,n)
+    POMD = sha1(join_result.encode('utf-8')).hexdigest()
+    return POMD
 
-def encode(phi_n):
-	global e
-	for i in range(2,phi_n):
-		if gcd(i,phi_n) == 1:
-			e = i
-			break
-	return e
+@app.route('/create_keys', methods=['POST'])
+def create_keys():
+    data = request.get_json()
+    key_len = int(data['key_len'])
+    (pub_key, pri_key) = rsa.newkeys(key_len)
+    
+    res = {'n': str(pri_key['n']), 'e': str(pri_key['e']), 'd': str(pri_key['d']), 'p': str(pri_key['p']), 'q': str(pri_key['q'])}
+    return res
 
-def decode(phi_n):
-	global e,d
-	for i in range(1,phi_n):
-		if (e*i)%phi_n == 1:
-			d = i
-			break
-	return d
-   
-def RSA(data):
-	global e,d
-	n = p*q
-	phi_n=(p-1)*(q-1)
-	e = encode(phi_n)
-	d = decode(phi_n)
-	return encrypt(data,e,n)
-
-
-@app.route('/success/<name>')
-def success(name):
-    return 'The digital Signature generated is %s' % name
-
-@app.route('/dual_signature', methods=['POST'])
-def dual_signature():
-    # if request.method=='POST':
-        # ccard = int(request.form['creditcard'])
-        # cccv = int(request.form['cvv'])
-        # cexpiry = int(request.form['expiry'])
-        # print("=================================" + request.get_json)
-        # orderid1=int(request.get_json.orderid)
-        # print("=================================" + str(orderid1))
-        # productid=int(request.get_json().productid)
-        # ordercost1=int(request.get_json().ordercost)
-        # PI=ccard+cccv+cexpiry
-        # print('PI: ',PI)
-        # OI=orderid1+productid+ordercost1
-        # print('OI: ',OI)
-        # # PIMD=hash(PI)
-        # # print('PIMD: ',PIMD)
-        # OIMD=hash(OI)
-        # print('OIMD: ',OIMD)
-        # # PO=PIMD+OIMD
-        # # print('P0: ',PO)
-        # # POMD=hash(PO)
-        # # print('POMD: ',POMD)
-        # # digital_sign = RSA(POMD)
-        # # print('Digital Sign: ',digital_sign)
-        # # merchant(OI,PIMD,digital_sign)
-        # return OIMD
-        # return redirect(url_for('success',name=digital_sign))
-    return "Minh"
-    # else:
-    #     ccard=int(request.args.get('creditcard'))    
-    #     cccv=int(request.args.get('cvv'))
-    #     cexpiry=int(request.args.get('expiry'))
-    #     orderid=int(request.args.get('orderid'))
-    #     productid=int(request.args.get('productid'))
-    #     ordercost=int(request.args.get('ordercost'))
-    #     PI=ccard+cccv+cexpiry
-    #     OI=orderid+productid+ordercost
-    #     PIMD=hash(PI)
-    #     OIMD=hash(OI)
-    #     PO=PIMD+OIMD
-    #     POMD=hash(PO)
-    #     digital_sign=RSA(POMD)
-    #     return redirect(url_for('success',name=digital_sign))
-
-def merchant(OI,PIMD,DigiSign):
-	OIMD = hash(OI)
-	PO = PIMD+OIMD
-	POMD = hash(PO)
-	POMD1 = decrypt(DigiSign)
-	print('POMD: ',POMD)
-	print('POMD1: ',POMD1)
-
-	if (POMD == POMD1):
-		print('POMD is verified successfully.')
-	else:
-		print('POMD failed to verify.')
-
-api.add_resource(test, "/dual")
+@app.route('/rsa', methods=['POST'])
+def myRSA():
+    data = request.get_json()
+    n = int(data['n'])
+    e = int(data['e'])
+    pomd = data['pomd']
+    pub_key = rsa.PublicKey(n, e)
+    message = pomd.encode('utf8')
+    res = rsa.encrypt(message, pub_key)
+    print(res)
+    return res
 
 if __name__=='__main__':
     app.run(host='localhost', port=3000)
--- key.js
+++ key.js
@@ -1,21 +1,119 @@
-async function myFunction() {
-    console.log("**********************************")
-    let ord = document.getElementById("orderid").value
-    let pro = document.getElementById("productid").value
-    let ordcost = document.getElementById("ordercost").value
+async function SHA_Order() {
+    let proid = document.getElementById("proid").value
+    let info = document.getElementById("info").value
+    let cost = document.getElementById("cost").value
 
-    // let response = await fetch("http://localhost:3000/dual", {
-    //     method:"POST",
-    //     headers:{
-    //         "Content-Type": "application/json"
+    let response = await fetch("http://localhost:3000/sha_order", {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "productid": proid,
+            "info": info,
+            "cost": cost
+        })
+    })
+    console.log(response)
+    let data = await response.text()
+    console.log(data)
+    document.getElementById("oimd").value = data
+}
 
-    //     },
-    //     body:JSON.stringify({
-    //         "ord": ord,
-    //         "pro": pro,
-    //         "ordcost": ordcost
-    //     })
-    // })
-    // console.log(response)
-    document.getElementById("key-minh").value = "response"
+async function SHA_Payment() {
+    let name = document.getElementById("name").value
+    let cmnd = document.getElementById("cmnd").value
+    let card = document.getElementById("card").value
+
+    let response = await fetch("http://localhost:3000/sha_payment", {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "name": name,
+            "cmnd": cmnd,
+            "card": card
+        })
+    })
+    console.log(response)
+    let data = await response.text()
+    console.log(data)
+    document.getElementById("pimd").value = data
+}
+
+async function Join() {
+    let oimd = document.getElementById("oimd").value
+    let pimd = document.getElementById("pimd").value
+
+    let join_result = oimd + pimd
+
+    console.log(join_result)
+    document.getElementById("join_result").value = join_result
+}
+
+async function SHA_Join() {
+    let join_result = document.getElementById("join_result").value
+
+    let response = await fetch("http://localhost:3000/sha_join", {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "join_result": join_result
+        })
+    })
+    let data = await response.text()
+    console.log(data)
+    document.getElementById("pomd").value = data
+}
+
+async function createKeys() {
+    let key_len = document.getElementById("key_len").value
+    console.log(key_len)
+
+    let response = await fetch("http://localhost:3000/create_keys", {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "key_len": key_len
+        })
+    })
+    let data = await response.json()
+    console.log(data)
+    public_key = 'n: ' + data.n + '\n' + 'e: ' + data.e
+    private_key = 'd: ' + data.d
+    document.getElementById("public_key").value = public_key
+    document.getElementById("private_key").value = private_key
+}
+
+async function RSA() {
+    let pub_key = document.getElementById("public_key").value
+    let pri_key = document.getElementById("private_key").value
+    let pomd = document.getElementById("pomd").value
+
+    let i = pub_key.indexOf('\n')
+    let n = pub_key.substr(3, i - 3)
+    let e = pub_key.substr(i + 4,)
+    console.log(n)
+    console.log(e)
+    console.log(pomd)
+
+    let response = await fetch("http://localhost:3000/rsa", {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "n": n,
+            "e": e,
+            "pomd": pomd
+        })
+    })
+    let data = await response.text()
+    console.log(data)
+    document.getElementById("dual_signature").value = data
 }
